59f320947d6b6c94d49104d0fe6c0f75949e6c7f
Екатерина <ironsupergirl@gmail.com>
2024-09-09 11:37:38 +0300

    fix: import errorYandler

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 
 import { env } from './utils/env.js';
 import notFoundHandler from './middlewares/notFoundHandler.js';
-import errorHandler from './middlewares/errorHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
 import loggerHandler from './utils/loggerHandler.js';
 
 import contactsRouter from './routers/contacts.js';
